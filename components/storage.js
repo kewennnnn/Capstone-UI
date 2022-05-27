e059845b0af348eeb207b7266b58ba0faa05e8c6
kewennnnn <65531086+kewennnnn@users.noreply.github.com>
2022-05-27 18:44:14 +0800

    Modular storage functions, platelet data persists

---
+++ components/storage.js
@@ -0,0 +1,22 @@
+function getBMI() {
+    const bmi = localStorage.getItem("bmi");
+    console.log(bmi);
+    return bmi;
+}
+function setBMI() {
+    const bmi = document.getElementById("input-value").innerHTML;
+    console.log(bmi);
+    localStorage.setItem("bmi",bmi);
+}
+
+
+function getPlatelet() {
+    const platelet = localStorage.getItem("platelet");
+    console.log(platelet);
+    return platelet;
+}
+function setPlatelet() {
+    const platelet = document.getElementById("input-value").innerHTML;
+    console.log(platelet);
+    localStorage.setItem("platelet",platelet);
+}
