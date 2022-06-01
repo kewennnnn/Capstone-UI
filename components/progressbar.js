3c97cd247607dda33d7d2e4e896181183ce10fbd
kewennnnn <65531086+kewennnnn@users.noreply.github.com>
2022-06-01 12:38:13 +0800

    Progress bar changes with stage

--- components/progressbar.js
+++ components/progressbar.js
@@ -2,21 +2,49 @@ class ProgressBar extends HTMLElement {
     constructor() {
       super();
       this.current = 0;
-
-      // switch(this.current) {
-      //   case 1: 
-      //     this.
-      //     break; 
-          
-      // }
+      this.stage1 = "pb-stage ";
+      this.line1 = "pb-line ";
+      this.stage2 = "pb-stage ";
+      this.line2 = "pb-line ";
+      this.stage3 = "pb-stage ";
+      this.line3 = "pb-line ";
+      this.stage4 = "pb-stage ";
     }
   
     connectedCallback() {
       this.current = this.getAttribute("current");
-      this.color1 = "orange";
-      this.color2 = (this.current > 1) ? "orange" : "black";
-      this.color3 = (this.current > 2) ? "orange" : "black";
-      this.color4 = (this.current > 3) ? "orange" : "black";
+      
+      // style for stage 
+      if (this.current == 1) {
+        this.stage1 += "pb-current";
+      } else if (this.current > 1) {
+        this.stage1 += "pb-done";
+      } 
+      if (this.current == 2) {
+        this.stage2 += "pb-current";
+      } else if (this.current > 2) {
+        this.stage2 += "pb-done";
+      } 
+      if (this.current == 3) {
+        this.stage3 += "pb-current";
+      } else if (this.current > 3) {
+        this.stage3 += "pb-done";
+      } 
+      if (this.current == 4) {
+        this.stage4 += "pb-current";
+      } 
+
+      // style for lines 
+      if (this.current > 1) {
+        this.line1 += "pb-line-done";
+      } 
+      if (this.current > 2) {
+        this.line2 += "pb-line-done";
+      } 
+      if (this.current > 3) {
+        this.line3 += "pb-line-done";
+      } 
+
       this.render();
     }
     
@@ -27,22 +55,22 @@ class ProgressBar extends HTMLElement {
       this.innerHTML = `
       <div id="progress-bar-wrapper">
         <div id="progress-bar" class="pb">
-          <div class="pb-stage pb-done">
+          <div class="${this.stage1}">
             <img src="../media/bmi.png"/>
             <p>BMI</p>
           </div>
-          <div class="pb-line pb-line-done"></div>
-          <div class="pb-stage pb-current">
+          <div class="${this.line1}"></div>
+          <div class="${this.stage2}">
             <img src="../media/platelet.png"/>
             <p>Platelet</p>
           </div>
-          <div class="pb-line"></div>
-          <div class="pb-stage">
+          <div class="${this.line2}"></div>
+          <div class="${this.stage3}">
             <img src="../media/screening.png"/>
             <p>Screen</p>
           </div>
-          <div class="pb-line"></div>
-          <div class="pb-stage">
+          <div class="${this.line3}"></div>
+          <div class="${this.stage4}">
             <img src="../media/diagnosis.png"/>
             <p>Result</p>
           </div>
