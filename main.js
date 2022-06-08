4c9146c16472d8fae26eb3a9f1786dcbacc6ffcd
kewennnnn <65531086+kewennnnn@users.noreply.github.com>
2022-06-08 13:02:15 +0800

    Change screen size

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 480,
+    width: 1280,
+    height: 720,
     webPreferences: {
       // nodeIntegration: true,
       // contextIsolation: false,
